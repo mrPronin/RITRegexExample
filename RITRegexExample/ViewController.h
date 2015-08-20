e6178bc7305bf0262bce5b5c79646e7e7ddf5ac1
Alexander Pronin <pronin.alx@gmail.com>
2015-08-20 16:36:48 +0300

    Initial Commit

---
+++ RITRegexExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RITRegexExample
+//
+//  Created by Aleksandr Pronin on 8/20/15.
+//  Copyright (c) 2015 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RITRegexExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RITRegexExample
+//
+//  Created by Aleksandr Pronin on 8/20/15.
+//  Copyright (c) 2015 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
